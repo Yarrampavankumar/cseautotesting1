1fb0a5f9d70c9f19f5d2f0c01d3d72aec8b82755
Pavan Kumar <pavankumaryarram919@gmail.com>
2026-02-23 11:59:38 +0530

    message

---
+++ src/main/java/com/aditya/csejenkins1/cseAdd.java
@@ -0,0 +1,11 @@
+package com.aditya.csejenkins1;
+
+public class cseAdd {
+	public int add(int a,int b) {
+		return a+b;
+	}
+	public int mul(int a,int b) {
+		return a*b;
+	}
+
+}
---
+++ src/test/java/com/aditya/csejenkins1/cseAddTest.java
@@ -0,0 +1,22 @@
+package com.aditya.csejenkins1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class cseAddTest {
+	cseAdd c = new cseAdd();
+
+	@Test
+	void csetest() {
+		int result = c.add(2,3);
+		assertEquals(5,result);
+	}
+	
+	@Test
+	void csetest1() {
+		int result = c.mul(2,3);
+		assertEquals(6,result);
+	}
+
+}
